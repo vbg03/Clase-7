35397380f8a2c4cae4105a611d9f6ea46e55ca7e
Valeria Bowers Gutierrez <valeria.bowers@uao.edu.co>
2024-09-05 19:46:50 -0500

    condicionales for in

---
+++ condicionales/for-in.js
@@ -0,0 +1,14 @@
+const regalosDeNavidad={
+    play5: 2,
+    moto: 2,
+    casa: 4,
+    finca: 6
+}
+
+for(compras in regalosDeNavidad){
+    console.log(compras)
+}
+
+for(compras in regalosDeNavidad){
+    console.log(`${compras}: ${regalosDeNavidad}`)
+}
